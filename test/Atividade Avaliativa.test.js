d51bdd4b1f6f462515e0e969fe4bfdb8668fa1dd
Marcus Vinicus <mvbrazz@hotmail.com>
2021-11-11 17:14:43 -0300

    Erro fatura entre 2000 e 2500 e a data de um mês atrás

--- test/Atividade Avaliativa.test.js
+++ test/Atividade Avaliativa.test.js
@@ -23,5 +23,27 @@ describe('Utils', () => {
             expect(resposta.length).toBe(3);
 
         }); 
+        test('Se o valor da fatura estiver entre 2000 e 2500 e a data for menor ou igual ao mes passado', () => {
+
+            var clientes = [
+                {id:0,nome:"José",data:6,estado:"Paraná"},
+                {id:1,nome:"Marcos",data:2,estado:"Paraná"},
+                {id:2,nome:"Paulo",data:9,estado:"Paraná"},
+                {id:3,nome:"Alex",data:5,estado:"Paraná"},
+                {id:4,nome:"Carlos",data:1,estado:"Paraná"}
+            ];
+
+            var faturas = [
+                {codigo:0,valor:1800,data:12,cliente_id:0},
+                {codigo:1,valor:1000,data:3,cliente_id:1},
+                {codigo:2,valor:2700,data:8,cliente_id:2},
+                {codigo:3,valor:2400,data:4,cliente_id:3},
+                {codigo:4,valor:2200,data:12,cliente_id:4}
+            ];
+
+            var resposta = faturas.filter(FiltroMaior2000);
+            resposta = resposta.filter(FiltroGeral);
+            expect(resposta.length).toBe(4);
+        });
     });    
 });
